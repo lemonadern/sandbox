5c4eec7b0428b960c263c7c17f2ab548ee6dd74a
Taishi Naka <taishi.develop@gmail.com>
2024-05-15 23:24:14 +0900

    formData serialize

---
+++ formData.ts
@@ -0,0 +1,44 @@
+
+const body = {
+    name: 'John Doe',
+    query: {
+        version: 2,
+    },
+}
+
+const serializeToFormData = (body: any) => {
+    const fd = new FormData();
+    for (const name in body) {
+        fd.append(name, body[name]);
+    }
+    return fd;
+}
+
+const res = await fetch('https://echo-server.deno.dev/', {
+    method: 'POST',
+    body: serializeToFormData(body),
+    // headers: {
+    //     'Content-Type': 'multipart/form-data',
+    // },
+})
+
+console.log(`Status: ${res.status}`)
+console.log(await res.text())
+
+export {}
+
+// command: 
+// deno run --allow-net formData.ts
+//
+// Output:
+// deno run --allow-net formData.ts 
+// Status: 200
+// ------1845662902205484666338047257
+// Content-Disposition: form-data; name="name"
+
+// John Doe
+// ------1845662902205484666338047257
+// Content-Disposition: form-data; name="query"
+
+// [object Object]
+// ------1845662902205484666338047257--
